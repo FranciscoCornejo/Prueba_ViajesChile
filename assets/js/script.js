1805c29c31ad063bede9353a3844d47234d6d221
Francisco <francisco.cornejo1@mail.udp.cl>
2024-01-26 14:36:19 -0300

    Prueba Final M2 - Viajes Chile

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+$(function()
+{
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.map(function (tooltipTriggerEl) 
+    {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+
+    $("#enviarMensaje").on('click', function(){
+        alert("su solicitud fue enviada exitosamente...");
+    });
+});
